1acc0599b5bed462ad10ae6f62656dd0cc5dd49b
adrxdev <adrx.developer@gmail.com>
2025-07-09 16:03:56 +0800

    2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,5 @@
-// Example: layout.tsx or RootLayouT
 import React from "react";
 
-
 export default function Footer() {
-return (
-  <html lang="en">
-    <body>
-      <Header />
-      <main>{children}</main>
-    </body>
-  </html>
-);
+  return null;
 }
