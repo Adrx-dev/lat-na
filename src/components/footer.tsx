1a4e67f6433ad2182cf2683926f8561a0cdfe37f
adrxdev <adrx.developer@gmail.com>
2025-07-09 15:19:18 +0800

    das

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,41 +1,34 @@
 import React from "react";
+import { Gradient } from "./gradient";
 import Container from "./container";
 import { Button } from "./button";
-import { Gradient } from "./gradient";
 
 export default function Footer() {
   return (
     <footer>
-      <div
-        className="relative bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url(/images/footer-bg.jpg)" }} // <-- replace with your image path
-      >
-                {/* Optional: add overlay */}
-        <div className="absolute inset-0 bg-white/80 z-0" />
-
-        {/* Content */}
-        <Gradient className="relative z-10">
-          <Container className="relative py-16 text-center sm:py-24">
-            <div>
-              <p className="font-mono text-xs font-semibold uppercase tracking-widest text-gray-500">
+      <Gradient className="relative">
+        <div className="absolute inset-2 rounded-4xl bg-white/80" />
+        <Container className="relative py-16 text-center sm:py-24">
+          <div>
+            <p className="font-mono text-xs/5 font-semibold uppercase tracking-widest text-gray-500">
+              Get started
+            </p>
+            <p className="mx-auto mt-6 text-3xl text-gray-950 sm:text-5xl tracking-tight font-medium">
+              React to dive in ? <br />
+              Start your free trial today
+            </p>
+            <p className="mx-auto mt-6 max-w-xs text-sm/6 text-gray-500">
+              Get the cheat codes for selling and unlock your team&apos;s
+              revenue potential.
+            </p>
+            <div className="mt-6">
+              <Button className="w-full sm:w-auto" href="/login">
                 Get started
-              </p>
-              <p className="mx-auto mt-6 text-3xl sm:text-5xl font-medium tracking-tight text-gray-950 max-w-3xl">
-                Do you want to use new features? <br />
-                Login now!
-              </p>
-              <p className="mx-auto mt-6 max-w-md text-sm sm:text-base text-gray-600">
-                Login to use more freatures.
-              </p>
-              <div className="mt-6">
-                <Button className="w-full sm:w-auto" href="/login">
-                  Login
-                </Button>
-              </div>
+              </Button>
             </div>
-          </Container>
-        </Gradient>
-      </div>
+          </div>
+        </Container>
+      </Gradient>
     </footer>
   );
 }
