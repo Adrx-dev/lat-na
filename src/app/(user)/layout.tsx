12a8b355cedca1826f9f1e5e1054b7da5fa207d0
adrxdev <adrx.developer@gmail.com>
2025-07-09 16:19:25 +0800

    metadata

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -9,8 +9,11 @@ import Footer from "@/components/footer";
 export const metadata: Metadata = {
   title: "Home | Lakambini Archives",
   description:
-    "A platform for Grade XI Lakambini students to share academic insights, experiences, and stories that inspire our community.",
-};
+   "A platform for Grade XI Lakambini students to share academic insights, experiences, and stories that inspire our community.",
+    icons: {
+    icon: "/favicon.ico", // ← this is your favicon
+  },
+  };
 export default function RootLayout({
   children,
 }: Readonly<{
